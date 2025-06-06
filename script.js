a15552449b52ff28fe0dea5a997176dc4565f610
CommitMike <personal.mikebrown@gmail.com>
2025-06-06 17:06:23 +0100

    Human logic written

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-function getComputerChoice() {
+function getComputerChoice() {    
     const randomNum = Math.random();
-    
+
     if (randomNum < 0.33) {
-        return "rock";
+        return "Computer choice: rock";
     } else if (randomNum < 0.66) {
-        return "paper";
+        return "Computer choice: paper";
     } else {
-        return "scissors"
+        return "Computer choice: scissors"
     }
 }
 
+
+
+
+function getHumanChoice() {
+    const sign = prompt("Enter rock, paper or scissors (in lower case)");
+    const choice = sign.toLowerCase();
+
+     if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        return "Your choice: " + choice;
+    } else {
+        console.log("Incorrect value input.");
+        return null; 
+    }
+}
+
+console.log(getHumanChoice())
 console.log(getComputerChoice())
